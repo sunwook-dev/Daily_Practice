82b464e57aa5db1bf0ceaaf2de05a25238172083
sunwook <tjsdnr9@gmail.com>
2025-04-04 18:11:09 +0900

    0404_daily

---
+++ 20250403/0403.js
@@ -0,0 +1,8 @@
+const input = document.getElementById("input");	//input 태그를 가져옴
+const button = document.getElementById("button");	//button 태그를 가져옴
+const output = document.getElementById("output");	//output 태그를 가져옴
+
+button.addEventListener("click", function () {	//button 태그에 클릭 이벤트 리스너를 추가함
+  const value = input.value;
+  
+})
